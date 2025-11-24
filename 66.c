abfdc22941620f3a525ad0a7092a493e0d875163
Immanuel Dorneich <idorneich@andovereyeinstitute.com>
2025-11-24 12:06:50 -0500

    passed

---
+++ 66.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int i = digitsSize - 1;
+    while (i >= 0) {
+        if (digits[i] == 9) {
+            digits[i] = 0;
+        } else {
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+        }
+        i--;
+    }
+    digits = calloc(digitsSize + 1, sizeof(int));
+    digits[0] = 1;
+    *returnSize = digitsSize + 1;
+    return digits;
+}
