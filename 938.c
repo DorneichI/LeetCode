7e0effa2a972eea68a016592afe1939689e39f23
Immanuel Dorneich <idorneich@andovereyeinstitute.com>
2025-12-01 14:00:22 -0500

    passed

---
+++ 938.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int rangeSumBST(struct TreeNode* root, int low, int high) {
+    if (!root) return 0;
+    if (root->val >= low && root->val <= high) return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    
+}
