f5ee6583eaba81abc8c7b7d53f40d0bbf04993dc
Immanuel Dorneich <idorneich@andovereyeinstitute.com>
2025-12-04 10:16:17 -0500

    passed

--- 2211.c
+++ 2211.c
@@ -1,27 +1,18 @@
 int countCollisions(char* directions) {
-    int leftCar = -1; //-1 left, 0 stationary, >=1 right
+    int stopped = -1;
     int collisions = 0;
+    int right = 0;
     for (int i = 0; directions[i] != '\0'; i++) {
-        if (directions[i] == 'L') {
-            if (leftCar == 0) {
-                collisions += 1;
-            } else if (leftCar >= 1) {
-                collisions += leftCar + 1;
-                leftCar = 0;
-            }
+        if (directions[i] == 'L' && stopped == 0) {
+            collisions += right + 1;
+            right = 0;
         } else if (directions[i] == 'S') {
-            if (leftCar == -1) {
-                leftCar = 0;
-            } else if (leftCar >= 1) {
-                collisions += leftCar;
-                leftCar = 0;
-            }
-        } else {
-            if (leftCar <= 0) {
-                leftCar = 1;
-            } else {
-                leftCar++;
-            }
+            collisions += right;
+            right = 0;
+            stopped = 0;
+        } else if (directions[i] == 'R') {
+            stopped = 0;
+            right++;
         }
     }
     return collisions;
