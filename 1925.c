015a3938217a39914c1c89ae61e7aa44a8cbf5be
Immanuel Dorneich <idorneich@andovereyeinstitute.com>
2025-12-08 12:18:08 -0500

    passes better still not great

--- 1925.c
+++ 1925.c
@@ -1,23 +1,23 @@
 int countTriples(int n) {
+    int hashTableSize = n * n;
+    int* hashTable = malloc(hashTableSize * sizeof(int));
+    memset(hashTable, 0, hashTableSize * sizeof(int));
     int* squares = malloc(n * sizeof(int));
-    int i, j, k;
+    int i, j;
     for (i = 1; i <= n; i++) {
-        squares[i - 1] = pow(i, 2);
+        squares[i - 1] = i * i;
+        hashTable[squares[i - 1] - 1] = 1;
     }
-    int result = 0;
     int sum;
+    int result = 0;
     for (i = 0; i < n; i++) {
         for (j = i; j < n; j++) {
-            sum = squares[i] + squares[j];
-            for (k = i + 1; k < n; k++) {
-                if (sum == squares[k]) {
-                    if (j == i) result++;
-                    else result += 2;
-                    break;
-                }
-            }
+           sum = squares[i] + squares[j];
+           if (sum <= hashTableSize && hashTable[sum - 1] == 1) {
+            if (i == j) result++;
+            else result += 2;
+           }
         }
     }
-    free(squares);
     return result;
 }
