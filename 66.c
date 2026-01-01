fe3d8753db5c54c32e2c7b0ee398ed5ca536131f
DorneichI <immanuel.dorneich@gmail.com>
2026-01-01 14:07:30 +0100

    still passed, redid because daily problem and didnt realise already passed

--- 66.c
+++ 66.c
@@ -18,3 +18,23 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
     *returnSize = digitsSize + 1;
     return digits;
 }
+
+// /**
+//  * Note: The returned array must be malloced, assume caller calls free().
+//  */
+// int* plusOne(int* digits, int digitsSize, int* returnSize) {
+//     *returnSize = digitsSize;
+//     while (digits[--digitsSize] == 9 && digitsSize > 0) {
+//         digits[digitsSize] = 0;
+//     }
+//     digits[digitsSize]++;
+//     int* returnDigits;
+//     if (digits[digitsSize] > 9) {
+//         (*returnSize)++;
+//         returnDigits = malloc(*returnSize * sizeof(int));
+//         memset(returnDigits, 0, *returnSize * sizeof(int));
+//         returnDigits[0] = 1;
+//         return returnDigits;
+//     }
+//     return digits;
+// }
