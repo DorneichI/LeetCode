27bee806b81f8accf32fc7cea447c20b8ec425e8
DorneichI <immanuel.dorneich@gmail.com>
2026-01-14 13:19:09 -0500

    passed
    Refactored palindrome check to use slicing.

--- 9.py
+++ 9.py
@@ -2,8 +2,9 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        x = str(x)
-        rev_x = ""
-        for i in range(len(x)):
-            rev_x += x[len(x) - i - 1]
-        return x == rev_x
+        # x = str(x)
+        # rev_x = ""
+        # for i in range(len(x)):
+        #     rev_x += x[len(x) - i - 1]
+        # return x == rev_x
+        return str(x) == str(x)[::-1]
