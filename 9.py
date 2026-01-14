25fcb95a76857c8919cb8aa31f553838b7a05a8c
DorneichI <immanuel.dorneich@gmail.com>
2026-01-14 13:18:41 -0500

    passed badly

---
+++ 9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        x = str(x)
+        rev_x = ""
+        for i in range(len(x)):
+            rev_x += x[len(x) - i - 1]
+        return x == rev_x
