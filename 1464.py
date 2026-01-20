29fd29f2b20b57ed6eb9f40de6ca492a6ce816b9
DorneichI <immanuel.dorneich@gmail.com>
2026-01-19 22:55:54 -0500

    passed

--- 1464.py
+++ 1464.py
@@ -1,8 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        maximum = float('-inf')
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if (nums[i]-1)*(nums[j]-1) > maximum:
-                    maximum = (nums[i]-1)*(nums[j]-1)
-        return maximum
+        nums.sort()
+        nums.reverse()
+        return (nums[0]-1)*(nums[1]-1)
+        
+        # maximum = float('-inf')
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if (nums[i]-1)*(nums[j]-1) > maximum:
+        #             maximum = (nums[i]-1)*(nums[j]-1)
+        # return maximum
