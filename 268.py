3c7ff4e6aff34ad32bec62630825b21e59b03787
DorneichI <immanuel.dorneich@gmail.com>
2026-01-20 09:56:23 -0500

    passed

--- 268.py
+++ 268.py
@@ -1,7 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums)):
-            if nums[i] != i:
-                return i
-        return len(nums)
+        a = sum(nums)
+        b = sum(range(len(nums) + 1))
+        return b - a
+        # nums.sort()
+        # for i in range(len(nums)):
+        #     if nums[i] != i:
+        #         return i
+        # return len(nums)
