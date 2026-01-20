8508fae72932887f023dfbc5bd5395e7dd35739c
DorneichI <immanuel.dorneich@gmail.com>
2026-01-19 22:45:16 -0500

    passed tragically badly

---
+++ 1337.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        strength = []
+        for i in range(len(mat)):
+            print(strength)
+            if len(strength) < k:
+                heapq.heappush(strength, (-sum(mat[i]), -i))
+            elif (-sum(mat[i]), -i) > strength[0]:
+                heapq.heapreplace(strength, (-sum(mat[i]), -i))
+        output = []
+        for _ in range(len(strength)):
+            output.append(-heapq.heappop(strength)[1])
+        return output[::-1]
