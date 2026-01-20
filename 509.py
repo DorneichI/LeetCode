70b3f7a8e9af4e1c51463771e5adc67bc341373c
DorneichI <immanuel.dorneich@gmail.com>
2026-01-20 11:30:40 -0500

    better still bad

--- 509.py
+++ 509.py
@@ -1,16 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp = [-1 for _ in range(n + 1)]
+        if n == 0:
+            return 0
+        dp = [0, 1]
 
-        def fibHelp(n: int) -> int:
-            if dp[n] > 0:
-                return dp[n]
-            else:
-                if n == 0:
-                    return 0
-                elif n == 1:
-                    return 1
-                else:
-                    return fibHelp(n - 1) + fibHelp(n - 2)
+        while len(dp) <= n:
+            dp.append(dp[len(dp) - 1] + dp[len(dp) - 2])
+        
+        return dp[len(dp) - 1]
+        # dp = [-1 for _ in range(n + 1)]
 
-        return fibHelp(n)  
+        # def fibHelp(n: int) -> int:
+        #     if dp[n] > 0:
+        #         return dp[n]
+        #     else:
+        #         if n == 0:
+        #             return 0
+        #         elif n == 1:
+        #             return 1
+        #         else:
+        #             return fibHelp(n - 1) + fibHelp(n - 2)
+
+        # return fibHelp(n)  
