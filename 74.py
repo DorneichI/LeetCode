cf1248448c36356a1377626f4506bfad60a722b6
DorneichI <immanuel.dorneich@gmail.com>
2026-01-21 15:13:34 -0500

    passed

---
+++ 74.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        length = m * n
+
+        low = 0
+        high = length - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if matrix[mid // n][mid % n] == target:
+                return True
+            elif matrix[mid // n][mid % n] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
