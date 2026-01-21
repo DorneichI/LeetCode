31ebe68ee4cac8076d36248f4e02e5e2f1485c5d
DorneichI <immanuel.dorneich@gmail.com>
2026-01-21 15:03:13 -0500

    passed

---
+++ 34.py
@@ -0,0 +1,42 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        low = 0
+        high = len(nums) - 1
+        i = -1
+        while low <= high:
+            mid = low + (high - low) // 2
+            print(mid, nums[mid], target)
+            if nums[mid] == target:
+                i = mid
+                break
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        if i == -1:
+            return [-1, -1]
+        
+        low = 0
+        high = i
+        while low < high:
+            print(low, high)
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                high = mid
+            else:
+                low = mid + 1
+        left = low
+
+        low = i
+        high = len(nums) - 1
+        while low < high:
+            print(low, high)
+            mid = math.ceil(low + (high - low) / 2)
+            if nums[mid] == target:
+                low = mid
+            else:
+                high = mid - 1
+        right = high
+
+        return [left, right]
