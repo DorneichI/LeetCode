a5922568401175b970f68724dc9c7016652a1a8c
DorneichI <immanuel.dorneich@gmail.com>
2026-01-21 15:38:58 -0500

    passed

---
+++ 215.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for i in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, i)
+            elif i > heap[0]:
+                heapq.heapreplace(heap, i)
+        return heap[0]
