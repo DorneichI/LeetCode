d989dcdf4094bac6eeb9945e7ffb662ab726b03b
DorneichI <immanuel.dorneich@gmail.com>
2026-01-26 22:01:47 -0500

    redone for quest, noticed im an idiot for doing this in two loops, still passed

--- 1929.c
+++ 1929.c
@@ -2,14 +2,22 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* getConcatenation(int* nums, int numsSize, int* returnSize) {
-    *returnSize = numsSize * 2;
-    int *returnNums = malloc(*returnSize * sizeof(int));
-    int i;
-    for (i = 0; i < numsSize; i++) {
-        returnNums[i] = nums[i];
+    int* returnArray = malloc(2 * numsSize * sizeof(int));
+    for (int i = 0; i < numsSize; i++) {
+        returnArray[i] = nums[i];
+        returnArray[i + numsSize] = nums[i];
     }
-    for (i = 0; i < numsSize; i++) {
-        returnNums[i + numsSize] = nums[i];
-    }
-    return returnNums;
+    *returnSize = 2 * numsSize;
+    return returnArray;
 }
+//     *returnSize = numsSize * 2;
+//     int *returnNums = malloc(*returnSize * sizeof(int));
+//     int i;
+//     for (i = 0; i < numsSize; i++) {
+//         returnNums[i] = nums[i];
+//     }
+//     for (i = 0; i < numsSize; i++) {
+//         returnNums[i + numsSize] = nums[i];
+//     }
+//     return returnNums;
+// }
