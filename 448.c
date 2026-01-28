523a1bf0eaf5d6cb41ac7ea20699383e4586d601
DorneichI <immanuel.dorneich@gmail.com>
2026-01-27 21:13:54 -0500

    passed dreadfully

---
+++ 448.c
@@ -0,0 +1,68 @@
+void merge(int* arr, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int* leftArr = malloc(n1 * sizeof(int));
+    int* rightArr = malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+    for (j = 0; j < n2; j++) {
+        rightArr[j] = arr[mid + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i] < rightArr[j]) {
+            arr[k++] = leftArr[i++];
+        } else {
+            arr[k++] = rightArr[j++];
+        }
+    }
+    while (i < n1) {
+        arr[k++] = leftArr[i++];
+    }
+    while (j < n2) {
+        arr[k++] = rightArr[j++];
+    }
+}
+
+void mergeSort(int* arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+    mergeSort(nums, 0, numsSize - 1);
+    int unfound = 1;
+    int* returnArray = malloc(numsSize * sizeof(int));
+    *returnSize = 0;
+    for (int j = 0; j < numsSize; j++) {
+        if (nums[j] == unfound) {
+            unfound++;
+        } else if (nums[j] > unfound) {
+            while (nums[j] > unfound) {
+                returnArray[*returnSize] = unfound++;
+                *returnSize += 1;
+            }
+            unfound++;
+        } else {
+        }
+    }
+    while (unfound <= numsSize) {
+        returnArray[*returnSize] = unfound++;
+        *returnSize += 1;
+    }
+    return returnArray;
+}
