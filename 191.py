e1b59f7355809d8773a6ee14851b5e7fc07bf5b7
DorneichI <immanuel.dorneich@gmail.com>
2026-02-10 12:40:50 -0500

    passed

---
+++ 191.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        weight = 0
+        while n > 0:
+            if n % 2 != 0:
+                weight += 1
+            n = n // 2
+        return weight
