bb4bd88cb6929bd06983b7ed48cf44d701792fe9
DorneichI <immanuel.dorneich@gmail.com>
2026-02-09 22:49:37 -0500

    passed (found out about quickselect)

--- 215.py
+++ 215.py
@@ -1,10 +1,23 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for i in nums:
-            if len(heap) < k:
-                heapq.heappush(heap, i)
-            elif i > heap[0]:
-                heapq.heapreplace(heap, i)
-        return heap[0]
+    def findKthLargest(self, nums: List[int], k: int) -> int:        
+        pivot = random.choice(nums)
+
+        left = [x for x in nums if x < pivot]
+        equal = [x for x in nums if x == pivot]
+        right = [x for x in nums if x > pivot]
+
+        if k <= len(right):
+            return self.findKthLargest(right, k)
+        elif k <= len(right) + len(equal):
+            return pivot
+        else:
+            return self.findKthLargest(left, k - len(right) - len(equal))
+            
+        # heap = []
+        # for i in nums:
+        #     if len(heap) < k:
+        #         heapq.heappush(heap, i)
+        #     elif i > heap[0]:
+        #         heapq.heapreplace(heap, i)
+        # return heap[0]
 
