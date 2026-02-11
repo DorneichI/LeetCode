4e9163025cfdfef9462e6e3a4f9ea62bed7f36c8
DorneichI <immanuel.dorneich@gmail.com>
2026-02-11 13:38:49 -0500

    still passed badly

--- 39.py
+++ 39.py
@@ -1,20 +1,31 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
+        dp = [[] for _ in range(target)]
+        for i in candidates:
+            if i <= target:
+                dp[i - 1].append([i])
+        for i in range(target):
+            for j in dp[i]:
+                for k in candidates:
+                    if k >= j[len(j) - 1] and i + k < target:
+                        dp[i + k].append(j + [k])
+                  
+        return dp[target - 1]
+        # res = []
 
-        def backtrack(start, path):
-            total = sum(path)
-            if total == target:
-                res.append(path.copy())
-                return
-            elif total > target:
-                return
+        # def backtrack(start, path):
+        #     total = sum(path)
+        #     if total == target:
+        #         res.append(path.copy())
+        #         return
+        #     elif total > target:
+        #         return
             
-            for i in range(start, len(candidates)):
-                path.append(candidates[i])
-                backtrack(i, path)
-                path.pop()
+        #     for i in range(start, len(candidates)):
+        #         path.append(candidates[i])
+        #         backtrack(i, path)
+        #         path.pop()
 
-        backtrack(0, [])
+        # backtrack(0, [])
 
-        return res
+        # return res
