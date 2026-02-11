79db92a9a83d410d9b660bb191dc02e58040741c
DorneichI <immanuel.dorneich@gmail.com>
2026-02-11 13:07:17 -0500

    passed

--- 75.py
+++ 75.py
@@ -3,48 +3,71 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def merge(arr, low, mid, high):
-            n1 = mid - low + 1
-            n2 = high - mid
+        z = 0
+        o = 0
+        t = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                z += 1
+            elif nums[i] == 1:
+                o += 1
+            else:
+                t += 1
+        i = 0
+        while z > 0:
+            nums[i] = 0
+            z -= 1
+            i += 1
+        while o > 0:
+            nums[i] = 1
+            o -= 1
+            i += 1
+        while t > 0:
+            nums[i] = 2
+            t -= 1
+            i += 1
+        # def merge(arr, low, mid, high):
+        #     n1 = mid - low + 1
+        #     n2 = high - mid
 
-            lowArray = [0] * n1
-            for i in range(n1):
-                lowArray[i] = arr[low + i]
-            highArray = [0] * n2
-            for j in range(n2):
-                highArray[j] = arr[mid + 1 + j]
+        #     lowArray = [0] * n1
+        #     for i in range(n1):
+        #         lowArray[i] = arr[low + i]
+        #     highArray = [0] * n2
+        #     for j in range(n2):
+        #         highArray[j] = arr[mid + 1 + j]
 
-            i = 0
-            j = 0
-            k = low
+        #     i = 0
+        #     j = 0
+        #     k = low
 
-            while i < n1 and j < n2:
-                if lowArray[i] <= highArray[j]:
-                    arr[k] = lowArray[i]
-                    i += 1
-                else:
-                    arr[k] = highArray[j]
-                    j += 1
-                k += 1
+        #     while i < n1 and j < n2:
+        #         if lowArray[i] <= highArray[j]:
+        #             arr[k] = lowArray[i]
+        #             i += 1
+        #         else:
+        #             arr[k] = highArray[j]
+        #             j += 1
+        #         k += 1
 
-            while i < n1:
-                arr[k] = lowArray[i]
-                i += 1
-                k += 1
+        #     while i < n1:
+        #         arr[k] = lowArray[i]
+        #         i += 1
+        #         k += 1
             
 
-            while j < n2:
-                arr[k] = highArray[j]
-                j += 1
-                k += 1
+        #     while j < n2:
+        #         arr[k] = highArray[j]
+        #         j += 1
+        #         k += 1
 
-        def mergeSort(arr, low, high):
-            if low < high:
-                mid = (low + high) // 2
+        # def mergeSort(arr, low, high):
+        #     if low < high:
+        #         mid = (low + high) // 2
 
-                mergeSort(arr, low, mid)
-                mergeSort(arr, mid + 1, high)
+        #         mergeSort(arr, low, mid)
+        #         mergeSort(arr, mid + 1, high)
 
-                merge(arr, low, mid, high)
+        #         merge(arr, low, mid, high)
 
-        mergeSort(nums, 0, len(nums) - 1)
+        # mergeSort(nums, 0, len(nums) - 1)
