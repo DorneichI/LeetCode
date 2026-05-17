69a63858d7c74b6c628cfb4a13f21824614824f6
DorneichI <immanuel.dorneich@gmail.com>
2026-05-17 17:40:29 -0400

    passed awfully but O(N)

---
+++ 2784.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        base = len(nums) - 1
+        i = 1
+        while i <= base:
+            print(0, nums)
+            while nums[i - 1] != i:
+                print(1, nums)
+                if nums[i - 1] > base:
+                    return False
+                elif nums[i - 1] == nums[nums[i - 1] - 1]:
+                    if nums[i - 1] != base:
+                        return False
+                    elif nums[base] != base:
+                        tmp = nums[i - 1]
+                        nums[i - 1] = nums[base]
+                        nums[base] = tmp
+                        continue
+                if nums[i - 1] == nums[nums[i - 1] - 1]:
+                    return False
+                tmp = nums[i - 1]
+                nums[i - 1] = nums[tmp - 1]
+                nums[tmp - 1] = tmp
+            i += 1
+        return nums[base] == base
+
+                
